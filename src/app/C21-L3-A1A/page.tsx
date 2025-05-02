ebdd9620b7fa11cfd2461a5efb704ac211dd5af5
mohammad asad <asadkhan41376@gmail.com>
2025-05-02 12:20:24 +0530

    add new activity C21-L3-A1A

---
+++ src/app/C21-L3-A1A/page.tsx
@@ -0,0 +1,12 @@
+import LayOutC21L3A1A from '@/layOutC21-L3-A1A'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <LayOutC21L3A1A/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/layOutC21-L3-A1A/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import cercelData from "@/layOutC21-L3-A1A/cercleData.json";
+
+const LayOutC21L3A1A = () => {
+  const [dataIndex, setDataIndex] = useState<number[]>([]);
+  const handleClick = (index: number) => {
+   if(!dataIndex?.includes(index)){
+    setDataIndex((prev)=>[...prev,index])
+   }
+  };
+  return (
+    <div className="min-h-screen p-10 flex justify-center flex-col gap-20 items-center bg-[#F8FAFC]">
+   <h3 className="text-black font-bold text-3xl text-center">7 Cs of communication</h3>
+      <div className="w-[800px]  h-[600px] relative flex justify-center items-center ">
+        <div className="w-[250px] h-[250px] shadow-md shadow-black  flex justify-center items-center  border border-black bg-[#D9DFC6] rounded-full">
+        <h3 className="text-black font-bold text-2xl text-center">7 Cs of communication</h3>
+        </div>
+        {cercelData.map((item, index) => (
+          <div
+            key={index}
+            onClick={()=>handleClick(index)}
+            style={{
+              top: item.top,
+              left: item.left,
+              right: item.right,
+              bottom: item.bottom,
+              backgroundColor: item.bg,
+              scrollbarWidth:"none"
+            }}
+            className="absolute hover:scale-105 cursor-pointer  overflow-y-scroll    shadow-md shadow-black flex justify-center items-center  border border-black   min-h-[200px] min-w-[200px]  rounded-full"
+          >
+            {dataIndex.includes(index) ? (
+              <div className=" w-[150px] h-[150px] text-[18px]  pl-2  rounded-full  text-center ">
+                {item.text}
+              </div>
+            ) : (
+              <h4 className="font-bold text-xl">{item.name}</h4>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LayOutC21L3A1A;
