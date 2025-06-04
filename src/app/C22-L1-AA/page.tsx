5348cb412ce37b65d6f2f9001ff742fd69673aa3
mohammad asad <asadkhan41376@gmail.com>
2025-06-04 15:43:40 +0530

    Song of resilience

--- src/app/C22-L1-AA/page.tsx
+++ src/app/C22-L1-AA/page.tsx
@@ -6,15 +6,27 @@ const Page = () => {
   return (
     <div className="min-h-screen flex justify-start p-5 items-center flex-col gap-4 bg-[#F8FAFC]">
       <h2 className="min-h-[80px] text-center text-3xl font-bold text-black">
-      Song of resilience
+        Song of resilience
       </h2>
 
-      <iframe width="600" height="300" className="rounded-lg" src="https://www.youtube.com/embed/82bVT9Byyuk?si=BNYLiK_jzy7nqxFu" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-
+      <iframe
+        width="600"
+        height="300"
+        className="rounded-lg"
+        src="https://www.youtube.com/embed/82bVT9Byyuk?si=BNYLiK_jzy7nqxFu"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <h3 className="text-xl">Listen to this song and share the words that show resilience.</h3>
+      <textarea
+        className="min-h-[80px]  min-w-[500px]  outline-none border border-black rounded-lg text-center p-3"
+        placeholder="write here..."
+      />
       {!showSugg ? (
         <button
           onClick={() => setShowSugg(true)}
-          
           className={`text-white bg-violet-900 px-5 py-2 rounded-lg 
           cursor-pointer
            `}
